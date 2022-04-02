dc7921d1abb1c42aaafdafe22cf9fbf8a5b9d1ba
shuijing.zhang <shuijing.zhang@tuya.com>
2022-04-02 11:42:05 +0800

    添加新特性demo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { useState } from "react";
+import AutomaticBatching from "./demo/AutomaticBatching";
+import StartTransition from "./demo/StartTransition";
+import DeferredValue from "./demo/DeferredValue";
+import SuspenseCom from "./demo/SuspenseCom";
 
 function App() {
-  const [value, setValue] = useState(0);
-  return <button onClick={() => setValue(prev => prev + 1)}>{value}</button>;
+  // const [value, setValue] = useState(0);
+  // return <button onClick={() => setValue(prev => prev + 1)}>{value}</button>;
+  // return <AutomaticBatching />;
+  // return <StartTransition />;
+  // return <DeferredValue />;
+  return <SuspenseCom />;
 }
 
 export default App;
